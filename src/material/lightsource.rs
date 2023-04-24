b842e9a0d8dd77068bc1daedcde76933c0e34652
chelate1118 <gs21077@gs.hs.kr>
2023-04-24 17:49:35 +0900

    implement materials

--- src/material/lightsource.rs
+++ src/material/lightsource.rs
@@ -1,5 +1,31 @@
+use glam::Vec3;
+
+use crate::{ray::Ray, color::Color};
+use super::Material;
 
 #[derive(Clone, Copy)]
 pub(crate) struct LightSource {
-    
+    color: Color
+}
+
+impl LightSource {
+    pub(crate) fn new(color: Color) -> Self {
+        if color.r < 1.0 || color.g < 1.0 || color.b < 1.0 {
+            panic!("Light Source should be bright");
+        }
+
+        LightSource { color }
+    }
+}
+
+impl Material for LightSource {
+    fn reflect(&self, ray: Ray, normal: Vec3, point: Vec3) -> Ray {
+        Ray {
+            source: point,
+            direction: ray.direction,
+            color: ray.color.mix_color(self.color),
+            light_source: true,
+            reflect_count: 0
+        }
+    }
 }
--- src/material/normal.rs
+++ src/material/normal.rs
@@ -1,15 +1,50 @@
 use glam::Vec3;
-use crate::ray::Ray;
+use rand_distr::Normal as Gaussian;
+use crate::{ray::{Ray, Optics}, color::Color};
 use super::Material;
 
 #[derive(Clone, Copy)]
 pub(crate) struct Normal {
+    rough: Gaussian<f32>,
+    color: Color
+}
 
+impl Normal {
+    pub(crate) fn new(color: Color, rough: f32) -> Self {
+        if color.r > 1.0 || color.g > 1.0 || color.b > 1.0 {
+            panic!("Normal surface color value must be less than 1.0.");
+        }
+
+        Normal {
+            rough: Gaussian::new(0.0, rough).unwrap(),
+            color
+        }
+    }
 }
 
 impl Material for Normal {
-    fn reflect(&self, ray: Ray, normal: Vec3) -> Ray {
+    fn reflect(&self, ray: Ray, normal: Vec3, point: Vec3) -> Ray {
+        let source = point;
+        let mut direction = ray.direction.reflect_from(normal);
+        let color = ray.color.mix_color(self.color);
+        let light_source = ray.light_source;
+        let reflect_count = ray.reflect_count + 1;
         
-        todo!()
+        direction = direction.dispersion(self.rough);
+        while is_valid_reflect(ray.direction, direction, normal) {
+            direction = direction.dispersion(self.rough);
+        }
+
+        Ray {
+            source,
+            direction,
+            color,
+            light_source,
+            reflect_count
+        }
     }
+}
+
+fn is_valid_reflect(before: Vec3, after: Vec3, normal: Vec3) -> bool {
+    return before.dot(normal) * after.dot(normal) < 0.0;
 }
