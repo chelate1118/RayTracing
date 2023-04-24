bb7f214ffa8a8de5cc9a5d624a2dab45cf8e8f6a
chelate1118 <gs21077@gs.hs.kr>
2023-04-24 17:51:04 +0900

    implement ray

--- src/object/sphere.rs
+++ src/object/sphere.rs
@@ -18,17 +18,17 @@ impl Sphere {
 
 impl Object for Sphere {
     fn reach_point(&self, ray: Ray) -> Option<Vec3> {
-        let source_to_center: Vec3 = self.center - ray.get_source();
+        let source_to_center: Vec3 = self.center - ray.source;
 
         let light_dist: f32 = source_to_center
-            .reject_from_normalized(ray.get_direction())
+            .reject_from_normalized(ray.direction)
             .length();
 
         if light_dist > self.radius {
             return None;
         }
 
-        let dot_product: f32 = Vec3::dot(source_to_center, ray.get_direction());
+        let dot_product: f32 = Vec3::dot(source_to_center, ray.direction);
         let reach_distance: f32 = dot_product - (self.radius.powi(2) - light_dist.powi(2)).sqrt();
 
         if dot_product < 0.0 {
@@ -39,37 +39,9 @@ impl Object for Sphere {
             panic!("Ray can't start from inside of the sphere.");
         }
 
-        let reach_vector: Vec3 = ray.get_direction() * reach_distance;
+        let reach_vector: Vec3 = ray.direction * reach_distance;
 
-        return Some(ray.get_source() + reach_vector);
+        return Some(ray.source + reach_vector);
     }
 }
 
-mod test_sphere {
-    #![allow(unused_imports)]
-    use glam::Vec3;
-    use crate::object::Object;
-    use crate::object::sphere::Sphere;
-    use crate::ray::Ray;
-
-    #[test]
-    fn reach_point() {
-        let ray = Ray::new(Vec3::ZERO, Vec3::X * 3.0, 10.0);
-        let ray2 = Ray::new(Vec3::ZERO, Vec3::X * -1.0, 10.0);
-
-        let sphere = Sphere::new(
-            Vec3::new(4.0, 4.0, 0.0),
-            5.0
-        );
-
-        let sphere2 = Sphere::new(
-            Vec3::new(4.0, 4.0, 0.0),
-            1.0
-        );
-
-        assert_eq!(Some(Vec3::new(1.0, 0.0, 0.0)), sphere.reach_point(ray));
-        assert_eq!(None, sphere.reach_point(ray2));
-        assert_eq!(None, sphere2.reach_point(ray));
-        assert_eq!(None, sphere2.reach_point(ray2));
-    }
-}
