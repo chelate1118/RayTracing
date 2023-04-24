8ba3bb6e90e4a3b50dbcae00b13c62a00a5dbc3c
chelate1118 <gs21077@gs.hs.kr>
2023-04-24 17:50:46 +0900

    implement ray

--- src/ray.rs
+++ src/ray.rs
@@ -1,26 +1,62 @@
 #![allow(dead_code)]
 
 use glam::Vec3;
+use rand_distr::{Normal as Gaussian, Distribution};
+use crate::color::Color;
+use std::f32::*;
 
 #[derive(Clone, Copy)]
 pub(crate) struct Ray {
-    source: Vec3,
-    direction: Vec3,
-    bright: f32
+    pub(crate) source: Vec3,
+    pub(crate) direction: Vec3,
+    pub(crate) color: RayColor,
+    pub(crate) light_source: bool,
+    pub(crate) reflect_count: u32,
 }
 
 impl Ray {
-    pub fn new(source: Vec3, direction: Vec3, bright: f32) -> Self {
-        return Ray { source, direction, bright };
+    pub(crate) fn new(source: Vec3, direction: Vec3, color: RayColor) -> Self {
+        return Ray { source, direction, color, light_source: false, reflect_count: 0 };
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub(crate) enum RayColor {
+    R(f32),
+    G(f32),
+    B(f32)
+}
+
+impl RayColor {
+    pub(crate) fn mix_color(&self, surface_color: Color) -> RayColor {
+        match self {
+            RayColor::R(r) => RayColor::R(r * surface_color.r),
+            RayColor::G(g) => RayColor::G(g * surface_color.g),
+            RayColor::B(b) => RayColor::B(b * surface_color.b)
+        }
+    }
+}
+
+pub(crate) trait Optics {
+    fn reflect_from(self, normal: Vec3) -> Self;
+    fn dispersion(self, gaussian: Gaussian<f32>) -> Self;
+}
+
+impl Optics for Vec3 {
+    fn reflect_from(self, normal: Vec3) -> Self {
+        return self - self.reject_from(normal) * 2.0
     }
 
-    pub fn get_source(&self) -> Vec3 {
-        return self.source;
-    }
-    pub fn get_direction(&self) -> Vec3 {
-        return self.direction.normalize();
-    }
-    pub fn get_bright(&self) -> f32 {
-        return self.bright;
+    fn dispersion(self, rough: Gaussian<f32>) -> Self {
+        const TWO_PI: f32 = 2.0 * consts::PI;
+        let (unit_x, unit_y) = self.any_orthonormal_pair();
+
+        let r_disp = rough.sample(&mut rand::thread_rng());
+        let angle = rand::random::<f32>() * TWO_PI;
+
+        let x_coor = r_disp * angle.sin() * self.abs();
+        let y_coor = r_disp * angle.cos() * self.abs();
+
+        return self + x_coor * unit_x + y_coor * unit_y;
     }
 }
