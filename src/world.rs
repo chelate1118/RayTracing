84f11f4d24b22e1a8c4edc5efbf5d689bfcab616
chelate1118 <gs21077@gs.hs.kr>
2023-04-25 08:26:25 +0900

    implement ray starting

--- src/world.rs
+++ src/world.rs
@@ -1,5 +1,33 @@
-use crate::object::Object;
+use crate::{object::Object, ray::Ray};
 
-struct World {
-    objects: Vec<Box<dyn Object>>
+pub(crate) struct World {
+    pub(crate) objects: Vec<Box<dyn Object>>
+}
+
+impl World {
+    pub(crate) fn start_ray(&self, ray: Ray) {
+        let mut ray = ray;
+
+        while !ray.is_done() {
+            match self.reach_object(ray) {
+                Some(obj) => ray = obj.reflect(ray),
+                None => break
+            }
+        }
+    }
+
+    fn reach_object(&self, ray: Ray) -> Option<&Box<dyn Object>> {
+        let mut reach_object = None;
+        let mut min_distance = std::f32::MAX;
+        for object in self.objects.iter() {
+            if let Some(distance) = object.reach_distance(ray) {
+                if min_distance > distance {
+                    min_distance = distance;
+                    reach_object = Some(object);
+                }
+            }
+        }
+
+        reach_object
+    }
 }
