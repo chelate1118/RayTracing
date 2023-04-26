c0f8a3a0e1945ad143bb21d5ee403ce1adc3010f
chelate1118 <gs21077@gs.hs.kr>
2023-04-26 14:35:01 +0900

    implement camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,11 +1,16 @@
 use glam::Vec3;
 use serde::{Serialize, Deserialize};
 
+use crate::{world::World, ray::{RayColor, Ray}};
+
 pub(crate) struct Camera {
     pub(crate) position: Vec3,
     pub(crate) direction: Vec3,
     pub(crate) width: usize,
-    pub(crate) height: usize
+    pub(crate) height: usize,
+    pub(crate) distance: f32,
+    screen_unit_x: Vec3,
+    screen_unit_y: Vec3
 }
 
 impl Camera {
@@ -17,18 +22,69 @@ impl Camera {
 
         Camera {
             position: Vec3::new(ci.x, ci.y, ci.z),
-            direction: Vec3::new(
-                pi.cos() * theta.sin(),
-                pi.sin() * theta.sin(),
-                theta.cos()
-            ),
+            direction: Self::get_direction(ci),
             width: ci.width,
-            height: ci.height
+            height: ci.height,
+            distance: ci.distance,
+            screen_unit_x: Self::get_unit_x(ci),
+            screen_unit_y: Self::get_unit_y(ci)
         }
+    }
+
+    pub(crate) fn start_ray(
+        &self,
+        world: World,
+        x: usize,
+        y: usize,
+        ray_color: RayColor
+    ) -> RayColor {
+        world.start_ray(self.generate_ray(x, y, ray_color)).color
+    }
+
+    fn generate_ray(
+        &self,
+        x: usize,
+        y: usize,
+        ray_color: RayColor
+    ) -> Ray {
+        let x_pix_dist = (x as i32 - (self.width/2) as i32) as f32 + 0.5;
+        let y_pix_dist = (y as i32 - (self.height/2) as i32) as f32 + 0.5;
+
+        let x_dist = x_pix_dist / self.distance * self.screen_unit_x;
+        let y_dist = y_pix_dist / self.distance * self.screen_unit_y;
+
+        let direction = self.direction + x_dist + y_dist;
+
+        Ray::new(self.position, direction, ray_color)
+    }
+
+    fn get_direction(ci: CameraInfo) -> Vec3 {
+        let pi = ci.pi.to_radians();
+        let theta = ci.theta.to_radians();
+
+        Vec3::new(
+            pi.cos() * theta.sin(),
+            pi.sin() * theta.sin(),
+            theta.cos()
+        )
+    }
+
+    fn get_unit_x(ci: CameraInfo) -> Vec3 {
+        Vec3::cross(
+            Self::get_direction(ci),
+            Vec3::Z
+        ).normalize()
+    }
+
+    fn get_unit_y(ci: CameraInfo) -> Vec3 {
+        Vec3::cross(
+            Self::get_direction(ci),
+            Vec3::X,
+        ).normalize()
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Copy)]
 struct CameraInfo {
     x: f32,
     y: f32,
@@ -36,11 +92,17 @@ struct CameraInfo {
     pi: f32,
     theta: f32,
     width: usize,
-    height: usize
+    height: usize,
+    distance: f32
 }
 
 impl CameraInfo {
     fn from_str(json: &str) -> Self {
-        serde_json::from_str(json).unwrap()
+        let ret: CameraInfo = serde_json::from_str(json).unwrap();
+        
+        assert!(ret.width % 2 == 0);
+        assert!(ret.height % 2 == 0);
+
+        ret
     }
 }
