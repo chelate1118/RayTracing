1a6a54945f430c8384b2338452e3a6bec61ae826
chelate1118 <gs21077@gs.hs.kr>
2023-04-26 19:56:44 +0900

    add normal_sphere

---
+++ src/object/sphere/normal_sphere.rs
@@ -0,0 +1,66 @@
+use glam::Vec3;
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
+
+use crate::{material::{normal::Normal, color::Color, Material}, object::Object, ray::Ray, loader::FromValue};
+
+use super::Sphere;
+
+
+#[derive(Clone, Copy)]
+pub(crate) struct NormalSphere {
+    sphere: Sphere,
+    material: Normal
+}
+
+impl FromValue for NormalSphere {
+    fn from_value(value: Value) -> serde_json::Result<Self> {
+        let nsi: NormalSphereInfo = serde_json::from_value(value)?;
+
+        Ok(NormalSphere {
+            sphere: Sphere::new(
+                Vec3::new(nsi.x, nsi.y, nsi.z),
+                nsi.radius
+            ),
+            material: Normal::new(
+                Color::new(nsi.r, nsi.g, nsi.b),
+                nsi.rough
+            )
+        })
+    }
+}
+
+impl NormalSphere {
+    pub(crate) fn new(center: Vec3, radius: f32, color: Color, rough: f32) -> Self {
+        NormalSphere {
+            sphere: Sphere::new(center, radius),
+            material: Normal::new(color, rough)
+        }
+    }
+}
+
+impl Object for NormalSphere {
+    fn reach_point(&self, ray: Ray) -> Option<Vec3> {
+        self.sphere.reach_point(ray)
+    }
+
+    fn reach_normal(&self, ray: Ray) -> Vec3 {
+        self.sphere.reach_normal(ray)
+    }
+
+    fn get_material(&self) -> Box<dyn Material> {
+        Box::new(self.material)
+    }    
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct NormalSphereInfo {
+    x: f32,
+    y: f32,
+    z: f32,
+    radius: f32,
+    r: f32,
+    g: f32,
+    b: f32,
+    rough: f32
+}
