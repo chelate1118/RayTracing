f45dcd95da51974a1d502df6217461e194192dd0
chelate1118 <gs21077@gs.hs.kr>
2023-04-26 15:04:09 +0900

    add macro assert_eq_vec3

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,3 +1,19 @@
 mod test_ray;
 mod test_shpere;
 mod test_camera;
+
+#[allow(unused_macros)]
+macro_rules! assert_eq_vec3 {
+    ($v1: expr, $v2: expr) => {
+        {
+            use glam::Vec3;
+
+            const APPROXIMATE_VALUE: f32 = 1e-6;
+
+            assert!(Vec3::distance($v1, $v2) < APPROXIMATE_VALUE);
+        }
+    };
+}
+
+#[cfg(test)]
+pub(crate) use assert_eq_vec3;
