69a6ceab9aba5e1366c406d342cee65efbb1cfca
chelate1118 <gs21077@gs.hs.kr>
2023-05-04 00:22:21 +0900

    seperate render directory

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,27 @@ mod material;
 mod loader;
 mod map;
 mod export;
+mod math;
 mod test;
+
+use std::fs;
 
 use loader::FromValue;
 
 pub struct HyperParameter {}
 impl HyperParameter {
-    const REFLECTION_COUNT: u32 = 2;
+    const REFLECTION_COUNT: u32 = 12;
     const RENDER_COUNT: usize = 1024;
-    const EXPORT_FRAME: usize = 32;
-    const FILE_PATH: &str = "maps/map1.json";
+    const EXPORT_FRAME: usize = 8;
+    const FILE_NAME: &str = "map2";
 }
 
 fn main() {
+    fs::create_dir_all(format!("render/{}", HyperParameter::FILE_NAME)).expect("Fail to create directory.");
+
     let map = map::Map::from_value(
-        loader::file_to_value(HyperParameter::FILE_PATH)
-            .unwrap_or_else(|_| panic!("File {} doesn't exist.", HyperParameter::FILE_PATH))
+        loader::file_to_value(&format!("maps/{}.json", HyperParameter::FILE_NAME))
+            .unwrap_or_else(|_| panic!("File {} doesn't exist.", HyperParameter::FILE_NAME))
     ).expect("Fail to deserialize map informations.");
 
     let (width, height) = map.get_screen_size();
@@ -37,7 +42,7 @@ fn main() {
     for frame in 0..HyperParameter::RENDER_COUNT {
         if (frame+1) % HyperParameter::EXPORT_FRAME == 0 {
             let cnt = frame / HyperParameter::EXPORT_FRAME;
-            let file_name = format!("render/render_{}.png", cnt);
+            let file_name = format!("render/{}/render_{}.png", HyperParameter::FILE_NAME,cnt);
 
             export::screen_to_png(&screen, width, height, frame as i32, &file_name);
         }
